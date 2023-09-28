cbd758868190cd59492e93bf517767e076c4bb14
Péterfai Bence István <peterfaib22@ADY-NAGYATAD.SULINET.HU>
2023-09-28 10:32:51 +0200

    .

--- pitagorasz 2.py
+++ pitagorasz 2.py
@@ -7,7 +7,10 @@ c=math.sqrt(a*a+b*b)
 print(c)
 
 for a in range(1,100):
-    for b in range(1,100):
+    for b in range(1,100,):
         c=math.sqrt(a*a+b*b)
         if c%1==0:
-            print(a,b,c)
+            if b-a>=10:
+                print(a,b,int(c))
+
+
